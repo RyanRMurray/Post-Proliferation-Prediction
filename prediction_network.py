f4c5e7ad8951c9d0f7a6ccc4b9a3b02f4a8b0e08
Ryan Murray <ryanrmurray@hotmail.co.uk>
2021-08-02 11:47:20 +0100

    branch code done. up next: joint embedding

--- prediction_network.py
+++ prediction_network.py
@@ -3,6 +3,7 @@ import cv2
 import re
 import math
 import itertools
+import sys
 import os
 os.environ['CUDA_VISIBLE_DEVICES'] = "0"
 
@@ -16,7 +17,10 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.layers import Dense, Flatten, Embedding, LSTM, GRU
 from tensorflow.keras.models import Sequential
 
-def word_embedding(data : json):
+from typing import Tuple
+
+#create token sequences for our inputs, and get info for generating the lstm branch
+def tokenize_data(data : list) -> Tuple[list, int, int]:
     tweets = len(data)
     words = set()
     input_size = 0
@@ -49,15 +53,50 @@ def word_embedding(data : json):
     print()
     print("Generated Sequences")
 
-    return data
+    return (data, len(words), input_size)
 
-j = json.load(open('./Data Sets/200ktweets.json', 'r'))
 
-for (_,x) in zip(range(10), word_embedding(j)):
-    print(x['text'])
-    print(x['sequence'])
-    print()
+def lstm_branch(word_num, input_size):
+    t_branch = Sequential()
+    embedding = Embedding(word_num, 32, input_length=input_size, name='lstm_embedder')
+    t_branch.add(embedding)
+    t_branch.add(LSTM(32))
 
+    print("Generated lstm branch")
+    t_branch.summary()
+    return t_branch
+
+def cnn_branch():
+    inceptionresnet = tf.keras.applications.InceptionResNetV2()
+    inception_output = inceptionresnet.layers[-2].output
+    i_branch = tf.keras.Model(inputs = inceptionresnet.input, outputs = inception_output)
+
+    print("Generated cnn branch")
+
+    return i_branch
+
+def main():
+    #check input then load
+    if len(sys.argv) != 2:
+        print("Run using the following command:\n\tpython3 prediction_network.py <data set path>")
+        return
+    
+    data_dir = sys.argv[1]
+
+    if data_dir[-5:] != '.json' or not os.path.isfile(data_dir):
+        print("Please enter a path to a valid json file")
+        return
+
+    data = json.load(open(data_dir, 'r'))
+
+    #generate sequences
+    (data, word_count, text_input_length) = tokenize_data(data)
+
+    #generate branches
+    i_branch = cnn_branch()
+    t_branch = lstm_branch(word_count, text_input_length)   
+
+main()
 
 '''
 inceptionresnet = tf.keras.applications.InceptionResNetV2()
