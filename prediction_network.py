4e924bdf7f708dbaa20c31b4cf66e9505f134e81
Ryan Murray <ryanrmurray@hotmail.co.uk>
2021-08-07 15:26:12 +0100

    mild refactor, currently lstm isnt getting the right shape of input

--- prediction_network.py
+++ prediction_network.py
@@ -4,8 +4,12 @@ import re
 import math
 import itertools
 import sys
+from datetime import datetime
+from PIL import Image
 
 import os
+
+from tensorflow.keras import regularizers
 os.environ['CUDA_VISIBLE_DEVICES'] = "0"
 
 import argparse
@@ -28,8 +32,20 @@ from keras.utils.vis_utils import plot_model
 
 from typing import Tuple
 
+#details:
+#   day-of-week of post,
+#   time-of-day of post,
+#   author account age,
+#   author followers count,
+#   author following count,
+#   author tweet count,
+#   author in list count,
+#   author verified status
+calc_detail_vector_size = lambda x : 2 + ((2**x)*x) + x - (2**(x+1))
+DETAIL_FEATURES = calc_detail_vector_size(8)
+
 #create token sequences for our inputs, and get info for generating the lstm branch
-def tokenize_data(data : list) -> Tuple[list, int, int]:
+def tokenize_data(data : list) -> Tuple[list, int, int, Tokenizer]:
     tweets = len(data)
     words = set()
     input_size = 0
@@ -62,7 +78,7 @@ def tokenize_data(data : list) -> Tuple[list, int, int]:
     print()
     print("Generated Sequences")
 
-    return (data, len(words), input_size)
+    return (data, len(words), input_size, tokenizer)
 
 #adds layers as described in rt wars paper
 def pre_joint_embed_layers(inputs, fc1,fc2):
@@ -74,13 +90,10 @@ def pre_joint_embed_layers(inputs, fc1,fc2):
     
     return pre_joint
 
-def lstm_branch(word_num, input_size):
-    lstm = Sequential()
-    embedding = Embedding(word_num, 256 , input_length=input_size, name='lstm_embedder')
-    lstm.add(embedding)
-    lstm.add(LSTM(256))
-
-    t_branch = tf.keras.Model(inputs= lstm.input, outputs=pre_joint_embed_layers(lstm.output,512,256))
+def lstm_branch(word_num, text_input):
+    t_branch = Embedding(word_num, 256)(text_input)
+    t_branch = LSTM(256, dropout=0.3, kernel_regularizer=regularizers.l2(0.05))(t_branch)
+    t_branch = pre_joint_embed_layers(t_branch,512,256)
 
     #t_branch.summary()
     print("Generated lstm branch")
@@ -88,85 +101,113 @@ def lstm_branch(word_num, input_size):
     return t_branch
 
 def cnn_branch():
-    inceptionresnet = tf.keras.applications.InceptionResNetV2(include_top=False)
+    inceptionresnet = tf.keras.applications.InceptionResNetV2()
     
     for layer in inceptionresnet.layers:
         layer.trainable = False
 
     pre_joint = pre_joint_embed_layers(inceptionresnet.output,768,256)
 
-    #reshape
-    reshaped = Reshape((256,))(pre_joint)
+    i_branch = Flatten()(pre_joint)
 
-    i_branch = tf.keras.Model(inputs=inceptionresnet.input, outputs=reshaped)
-    
+    i_branch = tf.keras.Model(inputs=inceptionresnet.input, outputs=i_branch)
     #i_branch.summary()
     print("Generated cnn branch")
-    return i_branch
+    return (i_branch, inceptionresnet.input)
 
-def detail_input():
-    #details:
-    #   day-of-week of post,
-    #   time-of-day of post,
-    #   author account age,
-    #   author followers count,
-    #   author following count,
-    #   author tweet count,
-    #   author in list count,
-    #   author verified status
-    calc_detail_vector_size = lambda x : 2 + ((2**x)*x) + x - (2**(x+1))
-    input_size = calc_detail_vector_size(8)
+def build_model(data : str, word_count, text_input_length):
+    text_input = Input(shape=(text_input_length,))
 
-    d_branch = Input(shape=(input_size,))
-
-    print("Generated details branch")
-    return d_branch
-
-
-
-def build_model(data_dir : str):
-    data = json.load(open(data_dir, 'r'))
-
-    #generate sequences
-    (data, word_count, text_input_length) = tokenize_data(data)
-    
     #generate branches
-    i_branch = cnn_branch()
-    t_branch = lstm_branch(word_count, text_input_length)
-    d_branch = detail_input()
+    (i_branch, image_input) = cnn_branch()
+    t_branch = lstm_branch(word_count, text_input)
+    d_branch = Input(shape=(DETAIL_FEATURES,))
     #joint embedding and convolution
-    joint = concatenate([i_branch.output, t_branch.output])
-    joint = Dense(256)(joint)
-    joint = Dense(128)(joint)
+    joint = concatenate([i_branch.output, t_branch])
+    joint = Dense(256, kernel_regularizer=regularizers.l2(0.05))(joint)
+    joint = Dense(128, kernel_regularizer=regularizers.l2(0.05))(joint)
     
     #add details input
     joint = concatenate([joint, d_branch])
-    model = Dense(128)(joint)
+    model = Dense(128, kernel_regularizer=regularizers.l2(0.05))(joint)
     
     rnn_in = Input(shape=(None,1))
-    rnn = SimpleRNN(128, return_sequences=True, return_state=True)
+    rnn = SimpleRNN(128, kernel_regularizer=regularizers.l2(0.05), return_sequences=True, return_state=True)
     dist = TimeDistributed(Dense(1, activation='relu'))
-
 
     model, _ = rnn(rnn_in, initial_state=model)
     model = dist(model)
 
-    final = tf.keras.Model(inputs=[i_branch.input, t_branch.input, d_branch, rnn_in], outputs=model)
+    final = tf.keras.Model(inputs = [image_input, text_input, d_branch, rnn_in], outputs=model)
     print("Generated model")
     return final
-    #todo: joint embedding constraint function, save model and tokenizer
+
+#turns a tweet into an input. tokenizer is optional, in case data is already tokenized.
+def tweet_to_training_pair(tweet, image_directory, input_size, tokenizer=None):
+    #input is (([image],[text],[user data]), result)
+
+    #check for image, else produce blank image
+    image = np.zeros((1,299,299,3))
+    path = '{}/{}.jpg'.format(image_directory, tweet['id'])
+    if os.path.isfile(path):
+        image = np.asarray(Image.open(path))
+
+    #get tokenized text
+    if 'sequence' in tweet:
+        text = tweet['text']
+    else:
+        if tokenizer is None:
+            print('Please supply tokenizer for non-sequenced tweets')
+            raise Exception
+        tokens = np.array(
+            tokenizer.texts_to_sequences([tweet['text']])[0]
+        )
+        #we pad the left side like this because we're iterating on each json object
+        text = np.zeros(input_size)
+        text[-len(tokens):] = tokens
+    
+    text = np.array(text)
+
+
+    #get user data
+    posted = datetime.fromtimestamp(tweet['created_at'])
+    user_data = list(map(int, [
+        posted.weekday(),
+        (posted - posted.replace(hour=0,minute=0,second=0)).seconds,
+        (datetime.now() - datetime.strptime(tweet['author_data']['created_at'], '%Y-%m-%dT%H:%M:%S.000Z')).days / 30,
+        tweet['author_data']['public_metrics']['followers_count'],
+        tweet['author_data']['public_metrics']['following_count'],
+        tweet['author_data']['public_metrics']['tweet_count'],
+        tweet['author_data']['public_metrics']['listed_count'],
+        tweet['author_data']['verified']
+    ]))
+
+    products = []
+    for x in range(len(user_data)-1):
+        for y in range(x+1, len(user_data)):
+            products.append(user_data[x] * user_data[y])
+
+    user_features = np.array(user_data+products)
+
+    #get ground truth
+    truth = int(tweet['final_metrics']['retweet_count'])
+
+    return ((image, text, user_features, np.array([0])), truth)
 
 def main():
     args = vars(parser.parse_args())
-
+    
     if args['dataset'][-5:] != '.json' or not os.path.isfile(args['dataset']):
         print("Please enter a path to a valid json file")
         return
 
+    #get tokenized data and tokenizer.
+    (data, word_count, text_input_length, tokenizer) = tokenize_data(json.load(open(args['dataset'], 'r')))
+
     #load model, or create one if no directory supplied
     if args['model'] is None:
         #create and save model
-        model : tf.keras.Model = build_model(args['dataset']) 
+        model : tf.keras.Model = build_model(data, word_count, text_input_length) 
         print('Enter a name for this model: ')
         name = input()
         model._name = name
@@ -174,9 +215,16 @@ def main():
         print('Saved model to ./Models{}'.format(name))
     else:
         print('Loading model')
-        model = tf.keras.models.load_model(args['model'])
+        model : tf.keras.Model = tf.keras.models.load_model(args['model'])
+        print('Model loaded')
     
     plot_model(model, to_file='model_plot.png', show_shapes=True)
+    data = json.load(open('./Data Sets/test.json', 'r'))
+    x = tweet_to_training_pair(data[0], '.', text_input_length, tokenizer=tokenizer)
+
+    for z in x[0]:
+        print(type(z))
+    model(x[0])
 
 main()
 
